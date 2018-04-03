c528a38e6132a444eeb10056e5a773c3bd122a21
Koger Darden <mkogerd@gmail.com>
2018-04-03 11:11:32 -0500

    Add textfield and button that logs textfield on click

--- app/src/main/java/slowger/hwk4/MainActivity.java
+++ app/src/main/java/slowger/hwk4/MainActivity.java
@@ -2,12 +2,29 @@ package slowger.hwk4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Submit button */
+    public void submitLocation(View view){
+        // Do something in response to button
+        location = (EditText)findViewById(R.id.location);
+
+        req = https://maps.googleapis.com/maps/api/geocode/json
+      ?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA
+                &client=YOUR_CLIENT_ID
+                &signature=SIGNATURE
+
+        System.out.print("Text field contains: ");
+        System.out.println(location.getText());
     }
 }
